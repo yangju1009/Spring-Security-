b394c02931a2177a7778448baef242f099cb8b4a
Yangju1009 <145637278+yangju1009@users.noreply.github.com>
2024-02-07 00:47:00 +0900

    커스텀 로그인 추가

--- spring_security/src/main/java/com/project/security/CustomAccessDeniedHandler.java
+++ spring_security/src/main/java/com/project/security/CustomAccessDeniedHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -14,13 +16,17 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class CustomAccessDeniedHandler implements AccessDeniedHandler{
 
-	
+	private final Logger log = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
 	
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 			AccessDeniedException accessDeniedException) throws IOException, ServletException {
 		
+		log.error("Access Denied Handler");
 		
+		log.error("Redirect.....");
+		
+		response.sendRedirect("/accessError");
 		
 	}
 	
--- spring_security/src/main/java/com/project/test/CommonController.java
+++ spring_security/src/main/java/com/project/test/CommonController.java
@@ -1,9 +1,13 @@
 package com.project.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.project.security.CustomAccessDeniedHandler;
 
 import lombok.extern.log4j.Log4j;
 
@@ -11,12 +15,29 @@ import lombok.extern.log4j.Log4j;
 @Controller
 public class CommonController {
 	
+	private final Logger log = LoggerFactory.getLogger(CommonController.class);
 	
-	@GetMapping("accessError")
+	@GetMapping("/accessError")
 	public void accessDenied(Authentication auth, Model model) {
 		System.out.println("access Denied: " + auth);
 		
 		model.addAttribute("msg", "Access Denied");
 		
+	}
+	
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		
+		log.info("error: " + error);
+		log.info("logout: " + logout);
+		
+		if (error != null) {
+			model.addAttribute("error", "Login Error Check Your Account");
+		}
+		
+		if (logout != null) {
+			model.addAttribute("logout", "Logout!");
+			
+		}
 	}
 }
